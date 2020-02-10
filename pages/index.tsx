467730f7084afc4e6f5e3fb285c84d014133863d
boatnoodle <nattasit31@gmail.com>
2020-02-10 13:25:40 +0700

    :heavy_plus_sign: Add antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,44 @@
 import React from "react";
+import { Form, Icon, Input, Button } from "antd";
+
 import Layout from "../components/Layout";
+
 const Index: React.FunctionComponent = () => {
+  const handleSubmit = () => {};
+
   return (
     <Layout title="Home">
+      <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item>
+          <Input
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+      <Button type="primary">Primary</Button>
+      <Button type="danger">Danger</Button>
+
       <h1>Review here is coming soon. 👋</h1>
       <p>Test automated deploy.</p>
       <p>Edit test something changed!!!!</p>
