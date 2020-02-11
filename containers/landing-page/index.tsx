110dc010196fafc7070134a0d8a58558fdbe0727
boatnoodle <nattasit31@gmail.com>
2020-02-11 19:36:30 +0700

    :rocket: Change config circle for new private registry

--- containers/landing-page/index.tsx
+++ containers/landing-page/index.tsx
@@ -1,10 +1,6 @@
-import { FunctionComponent, Fragment } from 'react';
+import { FunctionComponent, Fragment } from "react";
 const LandingPage: FunctionComponent = () => {
-    return (
-        <Fragment>
-            home page
-      </Fragment>
-    );
-}
+  return <Fragment>home page</Fragment>;
+};
 
 export default LandingPage;
---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+import Layout from "../components/Layout";
+
+const About: FunctionComponent = () => {
+  return (
+    <Layout>
+      <h1>About page</h1>
+    </Layout>
+  );
+};
+export default About;
