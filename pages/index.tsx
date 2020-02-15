0626aa70cf2da266b9dc88befde84e9f192fd6dd
boatnoodle <nattasit31@gmail.com>
2020-02-15 11:48:15 +0700

    :sparkles: Setup graphql client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import { FunctionComponent } from "react";
-import { Layouts } from "../components/Layout";
-import { LandingPage } from '../containers/landing-page';
+import { Layouts } from "components/Layout";
+import { LandingPage } from "containers/landing-page";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
 const Index: FunctionComponent = () => {
+  const HELLO = gql`
+    query {
+      hello
+    }
+  `;
+
+  // the hook that calls the query.
+  const hello = useQuery(HELLO);
+  console.log(hello);
   return (
     <Layouts>
       <LandingPage />
