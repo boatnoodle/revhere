274293938986a30cceb3a610d4dccf1f884385d6
boatnoodle <nattasit31@gmail.com>
2020-02-16 03:46:40 +0700

    :rocket: Test final deploy

--- containers/landing-page/index.tsx
+++ containers/landing-page/index.tsx
@@ -1,12 +1,14 @@
-
-import { FunctionComponent, Fragment } from 'react';
-import { ReviewItems } from 'containers/landing-page/Components/ReviewItems';
+import { FunctionComponent, Fragment } from "react";
+import { ReviewItems } from "containers/landing-page/Components/ReviewItems";
 
 let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const LandingPage: FunctionComponent = () => {
-    return (
-        <ReviewItems title='Explore Hot Reviews' subTitle='Find out hot reviews in our systems' items={items}></ReviewItems>
-    );
-}
-
+  return (
+    <ReviewItems
+      title="Explore Hot Reviews(Test Deploy)"
+      subTitle="Find out hot reviews in our systems"
+      items={items}
+    ></ReviewItems>
+  );
+};
