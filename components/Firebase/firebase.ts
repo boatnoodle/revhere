191b44161bd847ab5020b73c00de9491cbbe16b3
boatnoodle <nattasit31@gmail.com>
2020-02-16 11:19:35 +0700

    :bug: Fix api key wrong

--- components/Firebase/firebase.ts
+++ components/Firebase/firebase.ts
@@ -5,12 +5,12 @@ import 'firebase/auth';
 import { message } from 'antd';
 
 const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    apiKey: 'AIzaSyCf-zMEDcKa8nFOn96jNm-0mmcPlcpABBs',
+    authDomain: 'revhere-51751.firebaseapp.com',
+    databaseURL: 'https://revhere-51751.firebaseio.com',
+    projectId: 'revhere-51751',
+    storageBucket: 'revhere-51751.appspot.com',
+    messagingSenderId: 'G-442W9NDKZE',
 };
 
 class Firebase {
