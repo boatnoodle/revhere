fef457b2d6fb9a34f0f55c320f87e37ff202b73c
boatnoodle <nattasit31@gmail.com>
2020-02-17 19:50:09 +0700

    :bug: Fix conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,20 @@ import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 
 const Index: FunctionComponent = () => {
-<<<<<<< Updated upstream
-    const HELLO = gql`
+    const USERS = gql`
         query {
-            hello
+            users {
+                name
+            }
         }
     `;
 
     // the hook that calls the query.
-    const hello = useQuery(HELLO);
+    const { data, loading, error } = useQuery(USERS);
     return (
         <Layouts>
             <LandingPage />
         </Layouts>
     );
-=======
-  const USERS = gql`
-    query {
-      users {
-        name
-      }
-    }
-  `;
-
-  // the hook that calls the query.
-  const { data, loading, error } = useQuery(USERS);
-  return (
-    <Layouts>
-      <LandingPage />
-    </Layouts>
-  );
->>>>>>> Stashed changes
 };
 export default Index;
