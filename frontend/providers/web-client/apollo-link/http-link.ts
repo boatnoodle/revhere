ddf09a9559651a75cfeed1164f70f904423a560a
boatnoodle <nattasit31@gmail.com>
2020-02-23 18:52:42 +0700

    ♻️ Change default api url

--- frontend/providers/web-client/apollo-link/http-link.ts
+++ frontend/providers/web-client/apollo-link/http-link.ts
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 import { createHttpLink } from 'apollo-link-http';
 
-const GRAPHQL_URL = process.env.GRAPHQL_URL || 'http://localhost:4000/graphql';
+const GRAPHQL_URL = process.env.GRAPHQL_URL || 'http://178.128.86.161/graphql';
 
 export const httpLink = createHttpLink({
   fetch: !process.browser && fetch, // Switches between unfetch & node-fetch for client & server.
