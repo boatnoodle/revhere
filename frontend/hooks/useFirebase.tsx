6c35e7f5c86c9464abfafe3a74dd7a1d3d062864
boatnoodle <nattasit31@gmail.com>
2020-02-23 18:47:15 +0700

    🔥 Remove comment

--- frontend/hooks/useFirebase.tsx
+++ frontend/hooks/useFirebase.tsx
@@ -1,9 +1,6 @@
 import React, { useContext } from 'react';
 import { FirebaseContext } from 'components/Firebase/context';
 
-/**
- * you can useSession only if your component is wrapped with <ProvideAuth>
- */
 export const useFirebase = () => {
   return useContext(FirebaseContext);
 };
