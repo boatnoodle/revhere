f55daa517ab3850de1e88ebc8955abf2361cae5f
boatnoodle <nattasit31@gmail.com>
2020-02-23 22:23:42 +0700

    Handle api url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 // Initialize dotenv library
-// require('dotenv').config();
+require('dotenv').config();
 
-// const webpack = require('webpack');
+const webpack = require('webpack');
 
 /* eslint-disable */
 const withLess = require('@zeit/next-less');
@@ -28,21 +28,21 @@ module.exports = withCSS(
     webpack(config, { isServer }) {
       //start env =======================================================================
       // Fixes npm packages that depend on `fs` module
-      // config.node = {
-      //   fs: 'empty',
-      // };
-      // /**
-      //  * Returns environment variables as an object
-      //  */
-      // const env = Object.keys(process.env).reduce((acc, curr) => {
-      //   acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-      //   return acc;
-      // }, {});
+      config.node = {
+        fs: 'empty',
+      };
+      /**
+       * Returns environment variables as an object
+       */
+      const env = Object.keys(process.env).reduce((acc, curr) => {
+        acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+        return acc;
+      }, {});
 
-      // /** Allows you to create global constants which can be configured
-      //  * at compile time, which in our case is our environment variables
-      //  */
-      // config.plugins.push(new webpack.DefinePlugin(env));
+      /** Allows you to create global constants which can be configured
+       * at compile time, which in our case is our environment variables
+       */
+      config.plugins.push(new webpack.DefinePlugin(env));
       //end env =======================================================================
       if (isServer) {
         const antStyles = /antd\/.*?\/style.*?/;
