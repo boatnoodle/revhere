6437175757cb740a3b53bd8f8bda1488e4bcbc62
boatnoodle <nattasit31@gmail.com>
2020-02-23 22:34:57 +0700

    Handle graphql url for prod

--- frontend/providers/web-client/apollo-link/http-link.ts
+++ frontend/providers/web-client/apollo-link/http-link.ts
@@ -1,7 +1,8 @@
 import fetch from 'isomorphic-unfetch';
 import { createHttpLink } from 'apollo-link-http';
 
-const GRAPHQL_URL = process.env.NODE_ENV === 'production' ? process.env.GRAPHQL_URL : 'http://localhost:4000/graphql';
+const GRAPHQL_URL =
+  process.env.NODE_ENV === 'production' ? 'http://178.128.86.161/graphql' : 'http://localhost:4000/graphql';
 
 export const httpLink = createHttpLink({
   fetch: !process.browser && fetch, // Switches between unfetch & node-fetch for client & server.
