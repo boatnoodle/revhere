bdbad5944f5ed5b4efb70c046097d3ec573e7450
boatnoodle <nattasit31@gmail.com>
2020-03-08 21:03:26 +0700

    ⚡️ Merge all new typeDefs

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,7 +1,16 @@
 import { mergeTypes } from "merge-graphql-schemas";
 import userSchema from "./user";
 import reviewSchema from "./review";
+import categoryReviewSchema from "./categoryReview";
+import tagSchema from "./tag";
+import metaDataSchema from "./metaData";
 
-const types = [userSchema, reviewSchema];
+const types = [
+  userSchema,
+  reviewSchema,
+  categoryReviewSchema,
+  tagSchema,
+  metaDataSchema
+];
 
 export default mergeTypes(types, { all: true });
