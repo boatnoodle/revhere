a33e0495911e47777fe6649a5a6fe188c330658c
boatnoodle <nattasit31@gmail.com>
2020-03-09 23:48:26 +0700

    ⚡️  Update user from firebase to db

--- frontend/components/Layout/Component/FirebaseUi/index.tsx
+++ frontend/components/Layout/Component/FirebaseUi/index.tsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+import { useMutation } from '@apollo/react-hooks';
 import { FirebaseContext } from 'components/Firebase';
+import { CREATE_OR_UPDATE_USER } from 'components/Authentication/graphql';
 
 export const FacebookUi = () => {
+  const [createOrupdateUser] = useMutation(CREATE_OR_UPDATE_USER);
   const firebase = useContext(FirebaseContext);
 
   // Configure FirebaseUI.
   const uiConfig = {
     callbacks: {
       signInSuccessWithAuthResult: function() {
+        createOrupdateUser();
         return true;
       },
     },
