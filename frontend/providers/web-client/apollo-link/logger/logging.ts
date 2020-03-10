c4c9fa70ee4927ea6038a1d8987d6951bb26554b
boatnoodle <nattasit31@gmail.com>
2020-03-10 07:05:30 +0700

    🎨 Format code

--- frontend/providers/web-client/apollo-link/logger/logging.ts
+++ frontend/providers/web-client/apollo-link/logger/logging.ts
@@ -1,34 +1,34 @@
 const bindToConsole = (consoleMethod, polyfill) => {
-    return consoleMethod ? consoleMethod.bind(console) : polyfill;
+  return consoleMethod ? consoleMethod.bind(console) : polyfill;
 };
 
 const logging = (() => {
-    let prefix = '';
+  let prefix = '';
 
-    const consoleLog = (...args) => {
-        console.log(prefix, ...args);
-    };
+  const consoleLog = (...args) => {
+    console.log(prefix, ...args);
+  };
 
-    const consoleError = (...args) => {
-        console.error(prefix, ...args);
-    };
+  const consoleError = (...args) => {
+    console.error(prefix, ...args);
+  };
 
-    const consoleGroup = (...args) => {
-        consoleLog(...args);
-        prefix += '> ';
-    };
+  const consoleGroup = (...args) => {
+    consoleLog(...args);
+    prefix += '> ';
+  };
 
-    const consoleGroupEnd = () => {
-        prefix = prefix.slice(0, -2);
-    };
+  const consoleGroupEnd = () => {
+    prefix = prefix.slice(0, -2);
+  };
 
-    return {
-        log: consoleLog,
-        error: consoleError,
-        group: bindToConsole(console.group, consoleGroup),
-        groupCollapsed: bindToConsole(console.groupCollapsed, consoleGroup),
-        groupEnd: bindToConsole(console.groupEnd, consoleGroupEnd),
-    };
+  return {
+    log: consoleLog,
+    error: consoleError,
+    group: bindToConsole(console.group, consoleGroup),
+    groupCollapsed: bindToConsole(console.groupCollapsed, consoleGroup),
+    groupEnd: bindToConsole(console.groupEnd, consoleGroupEnd),
+  };
 })();
 
 export default logging;
