bbeb2b37c61c02a201fd4f0f630e42cd6f29152d
boatnoodle <nattasit31@gmail.com>
2020-03-10 22:51:39 +0700

    ♻️ Change font in review page

--- frontend/containers/review/components/ReviewContent/index.tsx
+++ frontend/containers/review/components/ReviewContent/index.tsx
@@ -32,14 +32,14 @@ export const ReviewContent: FunctionComponent<Props> = ({ data }) => {
   }
 
   return (
-    <Container>
+    <Container className="tk-ibm-plex-thai-looped">
       <Title>{review.titleReview}</Title>
       <UserDetail
         imageUrl="https://scontent.furt3-1.fna.fbcdn.net/v/t1.0-9/s960x960/74396069_2594174023999170_8719079083155128320_o.jpg?_nc_cat=102&_nc_oc=AQlutKLKb3uNoRq709fD6_1rLDbaTcEIneNb1Srk4016I6lI9IlwFSHJ06jCVeymodc&_nc_ht=scontent.furt3-1.fna&_nc_tp=7&oh=dd756c038d1b68c9bac24b5fe85a4cdd&oe=5EFDF82A"
         title="Nattashit Moonchanabaht"
         subTitle="3 ชั่วโมงที่แล้ว"
       />
-      <div dangerouslySetInnerHTML={createMarkup()} />
+      <ContentBox dangerouslySetInnerHTML={createMarkup()} />
     </Container>
   );
 };
