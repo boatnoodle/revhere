adf83b39f2cc4060105ed977d23d83772439e795
yoss <yoss@fazwaz.com>
2020-03-17 22:11:40 +0700

    ⚡️ Update Landing page

--- frontend/containers/landing-page/index.tsx
+++ frontend/containers/landing-page/index.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent, Fragment } from 'react';
 import { TabPaneComponent } from './Components/TabPaneComponent';
-import { ListComponent } from './Components/List';
 import StandardGrid from 'components/Standard-grid';
 import Notification from 'components/Notification';
+import { ContactUs } from './Components/ContactUs';
+
 export const LandingPage: FunctionComponent = () => {
   return (
     <Fragment>
@@ -10,7 +11,7 @@ export const LandingPage: FunctionComponent = () => {
         title="ยินดีต้อนรับเข้าสู่ Revhere"
         description="Revhere เป็นเว็บบอร์ดน้องใหม่ ที่มีการจัดหมวดหมู่ตามความสนใจนั้นๆ คุณสามารถเขียน และแบ่งปันประสบการณ์ คำวิจารณ์ และบอกต่อสิ่งๆนั้น ให้กับชุมชนที่มีความสนใจเดียวกัน"
       />
-      <StandardGrid content={<ListComponent />} left={<TabPaneComponent />} />
+      <StandardGrid content={<ContactUs />} left={<TabPaneComponent />} />
     </Fragment>
   );
 };
