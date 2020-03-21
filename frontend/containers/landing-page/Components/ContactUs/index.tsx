83435118a45612dcee86e90dc4529a73f9dd6e23
yoss <yoss@fazwaz.com>
2020-03-21 20:42:50 +0700

    ✨ Add Copyright text

--- frontend/containers/landing-page/Components/ContactUs/index.tsx
+++ frontend/containers/landing-page/Components/ContactUs/index.tsx
@@ -1,9 +1,10 @@
-import React, { FunctionComponent } from 'react';
-import { Input } from 'antd';
-const { TextArea } = Input;
-import styled, { css } from 'styled-components';
+import React, { FunctionComponent, Fragment } from 'react';
 import IconFeedback from '../../../../assets/icons/icon-feedback.svg';
+import { Input } from 'antd';
+import styled, { css } from 'styled-components';
 import { OutlinePrimaryButton } from 'components/Button';
+import { CopyRightText } from 'components/copyRightText';
+const { TextArea } = Input;
 const BaseBoxCss = css`
   padding: 10px 20px;
   display: flex;
@@ -39,24 +40,34 @@ const TextAreaStyled = styled(TextArea)`
   border-radius: 13px;
 `;
 
+const CopyRightTextBox = styled.div`
+  text-align: center;
+  margin-top: 15px;
+`;
+
 export const ContactUs: FunctionComponent = () => {
   return (
-    <Container>
-      <BoxRow>
-        <IconFeedback />
-        <TitleText>ส่งความเห็นถึงเรา</TitleText>
-      </BoxRow>
-      <HrStyled />
-      <Box>
-        <div>
-          เรามีความยินดีอย่างยิ่ง ที่จะให้คุณมีส่วนร่วมใน การพัฒนาสถานที่แห่งนี้ และขอบคุณล่วงหน้า
-          สำหรับข้อเสนอแนะหรือความคิดเห็นของคุณ
-        </div>
-        <TextAreaStyled allowClear rows={5} placeholder="กรุณาเข้าสู่ระบบ" />
-        <div style={{ marginTop: '10px', textAlign: 'right' }}>
-          <OutlinePrimaryButton>ส่งข้อความ</OutlinePrimaryButton>
-        </div>
-      </Box>
-    </Container>
+    <Fragment>
+      <Container>
+        <BoxRow>
+          <IconFeedback />
+          <TitleText>ส่งความเห็นถึงเรา</TitleText>
+        </BoxRow>
+        <HrStyled />
+        <Box>
+          <div>
+            เรามีความยินดีอย่างยิ่ง ที่จะให้คุณมีส่วนร่วมใน การพัฒนาสถานที่แห่งนี้ และขอบคุณล่วงหน้า
+            สำหรับข้อเสนอแนะหรือความคิดเห็นของคุณ
+          </div>
+          <TextAreaStyled allowClear rows={5} placeholder="กรุณาเข้าสู่ระบบ" />
+          <div style={{ marginTop: '10px', textAlign: 'right' }}>
+            <OutlinePrimaryButton>ส่งข้อความ</OutlinePrimaryButton>
+          </div>
+        </Box>
+      </Container>
+      <CopyRightTextBox>
+        <CopyRightText />
+      </CopyRightTextBox>
+    </Fragment>
   );
 };
