b817c3f64d01fc77279d229ca1a0209a320680fe
boatnoodle <nattasit31@gmail.com>
2020-03-22 01:28:47 +0700

    ✨ Add review status for shared

---
+++ frontend/utils/revewStatus.ts
@@ -0,0 +1,11 @@
+export const STATUS_ENUM = {
+  DRAFT: 'draft',
+  PUBLISH: 'publish',
+  REMOVE: 'remove',
+};
+
+export const STATUS_TEXT = {
+  DRAFT: 'แบบร่าง',
+  PUBLISH: 'เผยเพร่แล้ว',
+  REMOVE: 'ถูกลบแล้ว',
+};
