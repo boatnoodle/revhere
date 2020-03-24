d3d505e2a12ea23aa432397a19256b7421a20e22
boatnoodle <nattasit31@gmail.com>
2020-03-24 08:44:37 +0700

    :tada: Call api to save feedbacks in clickup

--- frontend/components/ContactUsForm/index.tsx
+++ frontend/components/ContactUsForm/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import React, { FunctionComponent, Fragment } from 'react';
+import React, { FunctionComponent, Fragment, useState } from 'react';
 import IconFeedback from '../../assets/icons/icon-feedback.svg';
 import styled, { css } from 'styled-components';
 
-import { Input } from 'antd';
+import { Input, notification } from 'antd';
 import { OutlinePrimaryButton } from 'components/Button';
 import { CopyRightText } from 'components/CopyRightText';
 import fetch from 'isomorphic-unfetch';
@@ -49,38 +49,49 @@ const CopyRightTextBox = styled.div`
   margin-top: 15px;
 `;
 
-const sendFeedback = async () => {
-  const accessToken = process.env.CLICKUP_PERSONAL_TOKEN;
-  const feedbackListsId = process.env.CLICKUP_FEEDBACK_LISTS_ID;
-  const url = 'https://private-anon-2ee8b78d31-clickup20.apiary-proxy.com/api/v2/list/10002611/task';
-
-  try {
-    const body = {
-      name: 'Feedback ลูกค้า',
-      content: 'New Task Content',
-      tags: ['feedback'],
-      priority: 1,
-      date_created: '1567780450202',
-      notify_all: true,
-      parent: null,
-    };
-
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: 'pk_3665453_9WVOB8EUVLZFEDJ4B711MM51CPRCKUSO',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-
-    console.log(response);
-  } catch (error) {
-    console.log(error, 'error');
-  }
-};
-
 export const ContactUsForm: FunctionComponent = () => {
+  const [content, setContent] = useState('');
+  const handleChange = e => {
+    setContent(e.currentTarget.value);
+  };
+
+  const sendFeedback = async () => {
+    // const accessToken = process.env.CLICKUP_PERSONAL_TOKEN;
+    // const feedbackListsId = process.env.CLICKUP_FEEDBACK_LISTS_ID;
+    const url = 'https://private-anon-2ee8b78d31-clickup20.apiary-proxy.com/api/v2/list/10002611/task';
+
+    try {
+      const body = {
+        name: 'Feedback ลูกค้า',
+        content,
+        tags: ['feedback'],
+        priority: 1,
+        date_created: '1567780450202',
+        notify_all: true,
+        parent: null,
+      };
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: 'pk_3665453_9WVOB8EUVLZFEDJ4B711MM51CPRCKUSO',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (response?.status === 200) {
+        notification.open({
+          message: 'ความคิดเห็นของคุณทุกส่งเรียบร้อยแล้ว',
+          description: 'เรายินดีรับคำแนะนำของพวกคุณและนำไปปรับปรุง ให้คุณได้พบกับประสบการณ์ที่ดีที่สุด',
+        });
+        setContent('');
+      }
+    } catch (error) {
+      console.log(error, 'error');
+    }
+  };
+
   return (
     <Fragment>
       <Container>
@@ -94,7 +105,13 @@ export const ContactUsForm: FunctionComponent = () => {
             เรามีความยินดีอย่างยิ่ง ที่จะให้คุณมีส่วนร่วมใน การพัฒนาสถานที่แห่งนี้ และขอบคุณล่วงหน้า
             สำหรับข้อเสนอแนะหรือความคิดเห็นของคุณ
           </div>
-          <TextAreaStyled allowClear rows={5} placeholder="กรุณาเข้าสู่ระบบ" />
+          <TextAreaStyled
+            value={content}
+            onChange={e => handleChange(e)}
+            allowClear
+            rows={5}
+            placeholder="คำแนะนำ, ปัญหา, ติชม..."
+          />
           <div style={{ marginTop: '10px', textAlign: 'right' }}>
             <OutlinePrimaryButton onClick={sendFeedback}>ส่งข้อความ</OutlinePrimaryButton>
           </div>
